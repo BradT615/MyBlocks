262fd5cf5910cc7e04eefcbf7c5484288d79e33b
BradT615 <125227539+BradT615@users.noreply.github.com>
2025-03-10 09:49:12 -0400

    lint

--- src/app/(protected)/styles/page.tsx
+++ src/app/(protected)/styles/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React, { useState } from 'react';
 import { DashboardHeader } from '@/app/(protected)/_components/dashboard-header';
 import { Button } from '@/components/ui/button';
